7dba12fa5aa257bf2b8f4c140ab8636d65ce201e
chenjw <chenjw@chenjw-ubuntu.(none)>
2014-01-26 15:23:11 +0800

    1.0.1

--- core/java/com/chenjw/logger/Logger.java
+++ core/java/com/chenjw/logger/Logger.java
@@ -7,62 +7,74 @@ package com.chenjw.logger;
  * 
  */
 public class Logger {
-	public Logger() {
+    public Logger() {
 
-	}
+    }
 
-	public boolean isDebugEnabled() {
-		return true;
-	}
+    public static Logger getLogger(Class<?> clazz) {
+        return new Logger();
+    }
 
-	// /////////
+    public static Logger getLogger(String name) {
+        return new Logger();
+    }
 
-	public void trace(Object key) {
-		System.out.println(key);
-	}
+    public boolean isDebugEnabled() {
+        return true;
+    }
+    
+    public boolean isInfoEnabled() {
+        return true;
+    }
 
-	public void trace(Object key, Throwable cause) {
-		System.out.println(key);
-	}
+    // /////////
 
-	public void debug(Object key) {
-		System.out.println(key);
-	}
+    public void trace(Object key) {
+        System.out.println(key);
+    }
 
-	public void debug(Object key, Throwable cause) {
-		System.out.println(key);
-	}
+    public void trace(Object key, Throwable cause) {
+        System.out.println(key);
+    }
 
-	public void info(Object key) {
-		System.out.println(key);
-	}
+    public void debug(Object key) {
+        System.out.println(key);
+    }
 
-	public void info(Object key, Throwable cause) {
-		System.out.println(key);
-	}
+    public void debug(Object key, Throwable cause) {
+        System.out.println(key);
+    }
 
-	public void warn(Object key) {
-		System.out.println(key);
-	}
+    public void info(Object key) {
+        System.out.println(key);
+    }
 
-	public void warn(Object key, Throwable cause) {
-		System.out.println(key);
-	}
+    public void info(Object key, Throwable cause) {
+        System.out.println(key);
+    }
 
-	public void error(Object key) {
-		System.out.println(key);
-	}
+    public void warn(Object key) {
+        System.out.println(key);
+    }
 
-	public void error(Object key, Throwable cause) {
-		System.out.println(key);
-	}
+    public void warn(Object key, Throwable cause) {
+        System.out.println(key);
+    }
 
-	public void fatal(Object key) {
-		System.out.println(key);
-	}
+    public void error(Object key) {
+        System.out.println(key);
+    }
 
-	public void fatal(Object key, Throwable cause) {
-		System.out.println(key);
-	}
+    public void error(Object key, Throwable cause) {
+        System.out.println(key);
+    }
+
+    public void fatal(Object key) {
+        System.out.println(key);
+    }
+
+    public void fatal(Object key, Throwable cause) {
+        System.out.println(key);
+    }
 
 }
